ec25ef18f4ec19c279ba160516a5bc2ffb740a29
Michael Böhme <boehme.mic@gmail.com>
2019-08-13 14:17:05 +0200

    relative difference added

--- TESTING/boys_test.f90
+++ TESTING/boys_test.f90
@@ -23,9 +23,9 @@ program boys_test
         integer :: n, io_error, nv
         double precision :: t, res, tv, mv, deps
         
-        write(*,*) "-----------------------------------------------------------------------------------------------"
-        write(*,'(A5,4A22)') "N", "X", "calc. value", "ref. value", "diff"
-        write(*,*) "-----------------------------------------------------------------------------------------------"
+        write(*,*) "---------------------------------------------------------------------------------------------------------------------"
+        write(*,'(A5,5A22)') "N", "X", "calculated value", "reference value", "difference", "rel. difference"
+        write(*,*) "---------------------------------------------------------------------------------------------------------------------"
         
         open(unit=20,file='benchmark.values',status='old',action='read',iostat=io_error)
         if ( io_error .eq. 0 ) then
@@ -37,7 +37,11 @@ program boys_test
                         end if
                         
                         res = Boys_func(nv, tv)
-                        write(*,'(I5,4F22.16)') nv, tv, res, mv, res-mv
+                        if ( mv .gt. 0.0 ) then
+                                write(*,'(I5,5E22.14)') nv, tv, res, mv, res-mv, (res-mv)/mv
+                        else
+                                write(*,'(I5,4E22.14, A22)') nv, tv, res, mv, res-mv, "--"
+                        end if
                 end do
         else
                 write(*,*) "Error! Can't open 'benchmark.values'!"
@@ -45,6 +49,6 @@ program boys_test
         
         close(unit=20)
         
-        write(*,*) "-----------------------------------------------------------------------------------------------"
+        write(*,*) "---------------------------------------------------------------------------------------------------------------------"
                 
 end program
